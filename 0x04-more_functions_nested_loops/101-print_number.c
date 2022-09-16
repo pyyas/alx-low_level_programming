e989bfeb65f3005ccc55afe5211d84737f06a86b
pyyas <95765879+pyyas@users.noreply.github.com>
2022-09-17 00:35:38 +0300

    adding file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,37 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - prime numbers
- *
- * Return: 0
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
  */
 
-int main(void)
+void print_number(int n)
 {
-	long num = 612852475143;
-	long divisor = 2;
-	long larg_prim = 0;
 
-	while (num != 1)
+	int d = 1, i = 0, ii = 0;
+
+	if (n < 0)
 	{
-		if (num % divisor == 0)
-		{
-			num = num / divisor;
-			larg_prim = divisor;
-		}
-		divisor += 1;
+		_putchar('-');
+		n = -n;
 	}
-	printf("%ld\n", larg_prim);
-	return (0);
+
+	while (n / d != 0)
+	{
+		d *= 10;
+		i++;
+	}
+	d = d / 10;
+
+	while (ii < i)
+	{
+		_putchar('0' + n / d);
+		n = n - (n / d) * d;
+		d = d / 10;
+		ii++;
+	}
+
+	if (i == 0)
+		_putchar('0' + n);
+
 }
