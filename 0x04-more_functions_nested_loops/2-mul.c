8af0bf47ddb534b754cdab55fd44111fed39b50b
pyyas <95765879+pyyas@users.noreply.github.com>
2022-09-17 00:18:11 +0300

    adding file

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - checks my code
- *
- * Return: 0
+ * mul - returns the product of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
  */
 
-int main(void)
+
+int mul(int a, int b)
 {
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096));
-	return (0);
+return (a * b);
 }
