75b57daa9598d90b147111c0dd377e51756d4605
pyyas <95765879+pyyas@users.noreply.github.com>
2022-09-24 00:48:23 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,8 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
 int main(void)
 {
-	system("echo and that piece of art is useful\" - Dora Korpar, \2015-10-19");
+	system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
