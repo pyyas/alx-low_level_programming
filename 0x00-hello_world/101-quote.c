0ef3462352af9af819fdf35508058f8deb0ba58d
pyyas <95765879+pyyas@users.noreply.github.com>
2022-09-24 00:50:07 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+*printing without using printf or puts fun
+*
+*return 1
+*
+*/
+
 int main(void)
 {
 	system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19");
